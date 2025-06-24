ca3d6ba6aa2db191024366f9ebe4bf82c205153d
Muhammad Shahyan <m.shahyan.edu@gmail.com>
2025-06-24 02:01:13 -0400

    is_palindrome return value when input is not a string

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,14 @@
 """
 Validates strings as palindromes.
 """
+from collections import deque
+
+def is_palindrome(word):
+    try:
+        stringcheck = (word + 1)
+        if stringcheck == word + 1:
+            return ValueError
+        pass
+    except TypeError:
+        pass
+    pass
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,3 +1,8 @@
 """
 Tests the palindrome module
 """
+from collections import deque
+from palindrome import is_palindrome
+
+def test_palindrome_check():
+    assert is_palindrome(321) == ValueError
